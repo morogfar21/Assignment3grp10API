857b06d9eb544658e05e759e0bdd6e38dd535b7d
Marcus <cendinho2@hotmail.com>
2020-12-11 17:49:05 +0100

    changed route names to lowercase

--- routes/highscore.js
+++ routes/highscore.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const ctrlHighScore = require('../controllers/highscoresController');
 
 // Get highscore list
-router.get('/highscoreList', ctrlHighScore.highscoreList);
+router.get('/highscorelist', ctrlHighScore.highscoreList);
 
 // Add highscore to Db
-router.post('/addHighscore', ctrlHighScore.addHighscore);
+router.post('/addhighscore', ctrlHighScore.addHighscore);
 
 module.exports = router;
