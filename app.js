96e8dbb8b9e1ffb77190c4e0f007b95d283cc933
Marcus <cendinho2@hotmail.com>
2020-12-12 22:20:14 +0100

    .

--- app.js
+++ app.js
@@ -48,41 +48,41 @@ app.use(function(err, req, res, next) {
 });
 
 //Setting up webSocket
-// const ws = require('ws').Server
-// const http = require('http');
+const ws = require('ws').Server
+const http = require('http');
 
-// const server = http.createServer(function (request, response) {
-//   console.log((new Date()) + ' Received request for ' + request.url);
-//   response.writeHead(200, {'Content-Type': 'text/plain'});
-//   response.write("Welcome to Node.js on OpenShift!\n\n");
-//   response.end("Thanks for visiting us! \n");
-// });
+const server = http.createServer(function (request, response) {
+  console.log((new Date()) + ' Received request for ' + request.url);
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write("Welcome to Node.js on OpenShift!\n\n");
+  response.end("Thanks for visiting us! \n");
+});
 
-// var wss = new ws({
-//   server: server,
-// });
+var wss = new ws({
+  server: server,
+});
 
-//wss.handleUpgrade();
+wss.handleUpgrade();
 
-// const scoreModel = require('./models/highscore');
-// wss.on('connection' , webscocket =>  {
-//   console.log("New connection");
+const scoreModel = require('./models/highscore');
+wss.on('connection' , webscocket =>  {
+  console.log("New connection");
 
-//   wss.clients.forEach(client => {
-//     scoreModel.find(function (err, doc){
-//       console.log(doc)
-//       client.send(JSON.stringify(doc))
-//     })
-//   })
+  wss.clients.forEach(client => {
+    scoreModel.find(function (err, doc){
+      console.log(doc)
+      client.send(JSON.stringify(doc))
+    })
+  })
 
-//   webscocket.onmessage = (message) => {
-//     var object = JSON.parse(message.data)
+  webscocket.onmessage = (message) => {
+    var object = JSON.parse(message.data)
 
 
-//     console.log(object.token)
-//     console.log(object.score)
-//   }
-// });
+    console.log(object.token)
+    console.log(object.score)
+  }
+});
 
 // server.listen(process.env.PORT || 8080, function () {
 //   console.log((new Date()) + ' Server is listening on port 8080');
