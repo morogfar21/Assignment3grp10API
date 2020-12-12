20c68ce457b23780d67c8f0f308d3e0977087c2a
Marcus <cendinho2@hotmail.com>
2020-12-12 22:41:51 +0100

    js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const createError = require('http-errors');
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors')
+const http = require('http')
+const ws = require('ws')
 
 require('./models/db')
 
@@ -10,8 +12,19 @@ require('./models/db')
 const authenticationRouter = require('./routes/authentication');
 const highScoreRouter = require('./routes/highscore');
 
+
+
 //Setting up server
 const app = express();
+const httpserver = http.createServer(app);
+const wss = new ws.Server({
+  server: httpserver
+});
+
+httpserver.listen(process.env.PORT || 8080, function () {
+  console.log((new Date()) + ' Server is listening on port 8080');
+});
+
 app.use(cors({credentials: true, origin: true}));
 app.use(express.json());
 app.use(logger('dev'));
@@ -45,6 +58,29 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.json({ error: err.message });
+});
+
+//Setting up webSocket
+//wss.handleUpgrade();
+
+const scoreModel = require('./models/highscore');
+wss.on('connection' , webscocket =>  {
+  console.log("New connection");
+
+  wss.clients.forEach(client => {
+    scoreModel.find(function (err, doc){
+      console.log(doc)
+      client.send(JSON.stringify(doc))
+    })
+  })
+
+  webscocket.onmessage = (message) => {
+    var object = JSON.parse(message.data)
+
+
+    console.log(object.token)
+    console.log(object.score)
+  }
 });
 
 module.exports = app;
