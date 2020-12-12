28a621c7ebaf344cd73aaac3f3b3c22ee9f32dc0
Marcus <cendinho2@hotmail.com>
2020-12-12 21:44:36 +0100

    removed auth

--- routes/highscore.js
+++ routes/highscore.js
@@ -4,7 +4,7 @@ const ctrlHighScore = require('../controllers/highscoresController');
 const auth = require('../authentication/auth');
 
 // Get highscore list
-router.get('/highscorelist', auth.authenticateJWT ,ctrlHighScore.highscoreList);
+router.get('/highscorelist', ctrlHighScore.highscoreList);
 
 // Add highscore to Db
 router.post('/addhighscore', auth.authenticateJWT ,ctrlHighScore.addHighscore);
